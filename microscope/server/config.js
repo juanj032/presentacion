74d0fbbb92787cc6532b043996a7c1db5dc279cf
Juan Sepulveda <juanjsepulveda@gmail.com>
2013-12-06 05:08:45 -0400

    Add facebook login

---
+++ microscope/server/config.js
@@ -0,0 +1,6 @@
+Accounts.loginServiceConfiguration.remove({service: "facebook"});
+Accounts.loginServiceConfiguration.insert({
+  service: "facebook",
+  appId: "187011904834563",
+  secret: "b9d78c9de3ff80d2ce9c743d478b95b0"
+});
