70d2ea67f501afbd07b64055e8131be016f1d51c
Juan Sepulveda <juanjsepulveda@gmail.com>
2013-12-06 05:51:39 -0400

    Add post_submit template

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -6,10 +6,16 @@ Router.configure({
 
 Router.map(function() {
   this.route('postsList', {path: '/'});
+  
   this.route('postPage', {
       path: '/posts/:_id',
       data: function  () {
         return Posts.findOne({_id:this.params._id});
       }
     });
+
+  this.route('postSubmit', {
+    path: '/submit'
+  });
+
 });
