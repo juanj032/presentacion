84b20cf5f62bbb70b30b36a2a19fe6ae16cc4718
Juan Sepulveda <juanjsepulveda@gmail.com>
2013-12-06 04:21:28 -0400

    Addd routing to a single post

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -6,4 +6,10 @@ Router.configure({
 
 Router.map(function() {
   this.route('postsList', {path: '/'});
+  this.route('postPage', {
+      path: '/posts/:_id',
+      data: function  () {
+        return Posts.findOne({_id:this.params._id});
+      }
+    });
 });
